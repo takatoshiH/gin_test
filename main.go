c7d20e3adaf6f6e5c5c56ca16ecf8cd424d00a0b
引尻 貴俊 <>
2022-01-02 00:03:02 +0900

    routing書いた

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"gin_test/controller"
 	"github.com/gin-gonic/gin"
 )
 
@@ -9,8 +10,16 @@ func main() {
 
 	// ミドルウェア
 	engine.Use(middleware.RecordUaAndTime)
-
-	v1 := Book
+	bookEngine := engine.Group("/book")
+	{
+		v1 := bookEngine.Group("/v1")
+		{
+			v1.POST("/add", controller.BookAdd)
+			v1.GET("/list", controller.BookList)
+			v1.PUT("/update", controller.BookUpdate)
+			v1.DELETE("/delete", controller.BookDelete)
+		}
+	}
 
 	engine.Run(":3000")
 }
