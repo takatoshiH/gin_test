a0c5c8c58d093e70eb64d1a719638f3ffd5a47d8
引尻 貴俊 <>
2022-01-02 13:24:36 +0900

    modelを作った

--- main.go
+++ main.go
@@ -1,21 +1,13 @@
 package main
 
 import (
+	"gin_test/model"
 	"github.com/gin-gonic/gin"
 	"net/http"
 	"strconv"
 )
 
-// album represents data about a record album.
-type album struct {
-	ID     string  `json:"id"`
-	Title  string  `json:"title"`
-	Artist string  `json:"artist"`
-	Price  float64 `json:"price"`
-}
-
-// albums slice to seed record album data.
-var albums = []album{
+var albums = []model.Album{
 	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
 	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
 	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
@@ -23,13 +15,12 @@ var albums = []album{
 
 // HTMLを返すようにしたい
 func getAlbums(c *gin.Context) {
-	// jsonを返すようにしている
 	c.IndentedJSON(http.StatusOK, albums)
 }
 
 // albumをIDは自動でインクリメントするようにしたい
 func postAlbums(c *gin.Context) {
-	var newAlbum album
+	var newAlbum model.Album
 
 	//IDがincrementするようにする
 	newAlbumID := getLatestID() + 1
